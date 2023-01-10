d1956b33d294ba8d29abdca6ca175763bbe3e6e3
Clément GASTON <clement.gaston@viacesi.fr>
2023-01-10 20:11:36 +0100

    Update the UI of the main feature

--- Assets/Scripts/EventHandler.cs
+++ Assets/Scripts/EventHandler.cs
@@ -1,60 +1,71 @@
 using UnityEngine;
 using TMPro;
 
-public class EventHandler : MonoBehaviour {
+public class EventHandler : MonoBehaviour
+{
     public string[] instructions;
     public TextMeshProUGUI DisplayStep;
     public TextMeshProUGUI DisplayInstruction;
     public GameObject PrevButton;
     public GameObject NextButton;
-    public bool isDecoupeuseFound = false;
 
+    private bool isDecoupeuseFound = false;
     private int nbStep;
     private int curStep = 0;
 
-    private void Awake() {
+    private void Awake()
+    {
         nbStep = instructions.Length - 1;
         PrevButton.SetActive(false);
         SetStep();
     }
 
-    public void onDecoupeuseFound() {
-        if(!isDecoupeuseFound) {
+    public void onDecoupeuseFound()
+    {
+        if (!isDecoupeuseFound)
+        {
             isDecoupeuseFound = true;
             curStep++;
             SetStep();
         }
     }
 
-    public void NextStep() {
+    public void NextStep()
+    {
         curStep++;
 
-        if(curStep > 1 && !PrevButton.activeSelf) {
+        if (curStep > 1 && !PrevButton.activeSelf)
+        {
             PrevButton.SetActive(true);
         }
 
 
-        if(curStep == nbStep) {
+        if (curStep == nbStep)
+        {
             NextButton.SetActive(false);
         }
         SetStep();
     }
 
-    public void PrevStep() {
+    public void PrevStep()
+    {
         curStep--;
 
-        if(curStep == 1) {
+        if (curStep == 1)
+        {
             PrevButton.SetActive(false);
         }
-        
-        if(curStep < nbStep && !NextButton.activeSelf) {
+
+        if (curStep < nbStep && !NextButton.activeSelf)
+        {
             NextButton.SetActive(true);
         }
         SetStep();
     }
 
-    public void SetStep() {
-        DisplayStep.SetText(curStep.ToString());
+    public void SetStep()
+    {
+        DisplayStep.SetText("Etape n°" + curStep.ToString());
         DisplayInstruction.SetText(instructions[curStep]);
     }
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
-    public void LaunchZing() {
+public class MainMenu : MonoBehaviour
+{
+    public void LaunchZing()
+    {
         SceneManager.LoadScene("LaunchZing");
     }
 
-    public void InfoZing() {
+    public void InfoZing()
+    {
         SceneManager.LoadScene("InfoZing");
     }
 
-    public void QuitApp() {
+    public void QuitApp()
+    {
         Application.Quit();
     }
 }
