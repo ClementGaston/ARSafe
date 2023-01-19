f3e840ce6aa722da0fa7ae085ad2d00b6fe27e8f
Clément GASTON <clement.gaston@viacesi.fr>
2023-01-19 20:49:15 +0100

    Add tutorial

---
+++ Assets/Scripts/UIColorLerper.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIColorLerper : MonoBehaviour
+{
+    private float speed = 1f;
+    private Color startColor = Color.yellow;
+    private Color endColor = Color.black;
+    private bool repeatable = true;
+    private float startTime;
+
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (!repeatable)
+        {
+            float t = (Time.time - startTime) * speed;
+            GetComponent<Image>().color = Color.Lerp(startColor, endColor, t);
+        }
+        else
+        {
+            float t = Mathf.Sin((Time.time - startTime) * speed);
+            GetComponent<Image>().color = Color.Lerp(startColor, endColor, t);
+        }
+    }
+}
