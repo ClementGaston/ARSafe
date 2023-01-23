7742af4e6d94b58e52f3b3eef9977af3b55e5e61
Clément GASTON <clement.gaston@viacesi.fr>
2023-01-23 16:25:58 +0100

    Finished the missing mod

--- Assets/Scripts/Popup.cs
+++ Assets/Scripts/Popup.cs
@@ -2,10 +2,38 @@ using UnityEngine;
 
 public class Popup : MonoBehaviour
 {
+    public GameObject PrevBtn;
+    public GameObject CloseBtn;
+
+    public GameObject defaultMenu;
+    private GameObject content;
+
     public void Show(GameObject content)
     {
+        if(gameObject.activeSelf)
+        {
+            return;
+        }
+        
         gameObject.SetActive(true);
         content.SetActive(true);
+    }
+
+    public void SubShow(GameObject content)
+    {
+        defaultMenu.SetActive(false);
+        content.SetActive(true);
+        PrevBtn.SetActive(true);
+        CloseBtn.SetActive(false);
+        this.content = content;
+    }
+
+    public void Back(GameObject content)
+    {
+        defaultMenu.SetActive(true);
+        CloseBtn.SetActive(true);
+        PrevBtn.SetActive(false);
+        this.content.SetActive(false);
     }
 
     public void Hide()
